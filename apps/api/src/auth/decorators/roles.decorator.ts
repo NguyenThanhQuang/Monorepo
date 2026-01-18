c007b2405c1867167901ead714dcbbbe8b90f6b7
Kratos17122004 <nguyenthanhquang.21062005@gmail.com>
2026-01-18 12:17:34 +0700

    fix

--- apps/api/src/auth/decorators/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { ROLES_KEY, UserRole } from '@obtp/shared-types';
-
-// Chỉ nhận input là Enum UserRole, cấm string bậy bạ
-export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
--- packages/business-logic/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./auth/auth.logic";
-export * from "./constants";
-export * from "./utils/data.util";
-export * from "./utils/url.util";
