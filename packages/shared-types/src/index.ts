23c3b5d55f0eea8b593c94e495f8af71302a41c8
Kratos17122004 <nguyenthanhquang.21062005@gmail.com>
2026-01-19 21:57:59 +0700

    fix

--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./common";
+export * from "./dtos";
 export * from "./enums";
 export * from "./location";
+// export * from "./models";
--- packages/validation/src/index.ts
+++ packages/validation/src/index.ts
@@ -1,4 +1,4 @@
-export * from '../src/location.zod';
-export * from '../src/common.zod';
+export * from "../src/common.zod";
+export * from "../src/location.zod";
 export * from "./auth.zod";
 export * from "./common.zod";
