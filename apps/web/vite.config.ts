a7a52559d6e474283f1bac9d1b13e6d2ea424b02
Kratos17122004 <nguyenthanhquang.21062005@gmail.com>
2026-02-02 12:17:02 +0700

    fix

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // 1. Alias nội bộ của Web
+      "@": path.resolve(__dirname, "./src"),
+
+      // 2. FORCE VITE đọc code từ 'src' của packages thay vì 'dist'
+      "@obtp/shared-types": path.resolve(
+        __dirname,
+        "../../packages/shared-types/src/index.ts",
+      ),
+      "@obtp/validation": path.resolve(
+        __dirname,
+        "../../packages/validation/src/index.ts",
+      ),
+      "@obtp/api-client": path.resolve(
+        __dirname,
+        "../../packages/api-client/src/index.ts",
+      ),
+    },
+  },
+});
