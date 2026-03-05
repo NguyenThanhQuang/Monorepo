4aadb102e6c08555e4d204294c280125e646bc47
Kratos17122004 <nguyenthanhquang.21062005@gmail.com>
2026-03-05 10:07:47 +0700

    Revert "Update UI web"
    This reverts commit 78e0fcbe9149f53ddab19f133f93e303220a0592.

--- apps/web/src/components/layout/Header.tsx
+++ apps/web/src/components/layout/Header.tsx
@@ -108,7 +108,7 @@ export function Header({
           {/* LOGO */}
           <Link to="/" className="flex items-center space-x-3">
             <Bus className="w-8 h-8 text-blue-600" />
-            <span className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-teal-500">
+            <span className="text-xl font-bold bg-linear-to-r from-blue-600 to-teal-500 bg-clip-text text-transparent">
               Online Bus Ticket Platform
             </span>
           </Link>
